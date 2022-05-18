23eea94dd23bca672c9adf36c20752286e7c34ed
cluemaster-robert <79128397+cluemaster-robert@users.noreply.github.com>
2022-05-17 19:28:30 -0700

    Delete RoomController directory

--- RoomController/platform_facts.py
+++
@@ -1,3 +0,0 @@
-facts = ["Did you know clues can be automated from device triggers ? ",
-         "Did you know cluemaster can be triggered from control panel ? ",
-         "Did you know Escape Room app is developed using Python ? "]
