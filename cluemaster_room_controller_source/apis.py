6adc8faf04ab4581365f15320f0a01bcf7f6e1b8
Mrittunjoy Sarkar <63890677+hiransarkar21@users.noreply.github.com>
2022-09-04 22:22:33 +0530

    updating latest code from development repository

--- cluemaster_room_controller_source/apis.py
+++
@@ -1,6 +0,0 @@
-master_base_url = "https://devapi.cluemaster.io/"
-
-# apis available to cluemaster room controller
-GENERATE_API_TOKEN_API = "https://deviceapi.cluemaster.io/api/Auth/PostGenerateApiKey"
-ROOM_CONTROLLER_REQUEST_API = master_base_url + "api/roomcontroller/GetRoomControllerRequest/{device_id}"
-POST_ROOM_CONTROLLER_REQUEST = master_base_url + "api/roomcontroller/PostRoomControllerRequest/{device_id}/{request_id}"
