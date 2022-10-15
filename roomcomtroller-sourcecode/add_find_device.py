82f4889cddf80d237800e3c4c9f88e9f8dd5ba21
mrittunjoysarkar <mrittunjoysarkar21@gmail.com>
2022-10-15 14:03:43 +0530

    added two instance methods ip_connect and network_search. They are triggered based on the value of the argument 'method'

--- roomcomtroller-sourcecode/add_find_device.py
+++ roomcomtroller-sourcecode/add_find_device.py
@@ -7,19 +7,29 @@ import sys
 
 
 class AddFindDevices(threading.Thread):
-    def __init__(self):
+    def __init__(self, method):
         super(AddFindDevices, self).__init__()
 
         # global attributes
         self.active = None
+        self.method = method
 
     def run(self):
+        if self.method == 'add':
+            self.ip_connect()
+        else:
+            self.network_search()
+
+    def ip_connect(self):
+        pass
+
+    def network_search(self):
         pass
 
 
 def main():
     if __name__ == "__main__":
-        add_find_device_thread = AddFindDevices()
+        add_find_device_thread = AddFindDevices(method='add') # add value for the argument method 'add or find'
         add_find_device_thread.start()
 
 
