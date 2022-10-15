39e559124fc72ddabae537080df89d5e47d442b7
mrittunjoysarkar <mrittunjoysarkar21@gmail.com>
2022-10-15 14:14:58 +0530

    re: updated the way the thread decides the method to be used

--- roomcomtroller-sourcecode/add_find_device.py
+++ roomcomtroller-sourcecode/add_find_device.py
@@ -15,8 +15,8 @@ class AddFindDevices(threading.Thread):
         self.method = method
 
     def run(self):
-        if self.method[0][0] == 'add':
-            self.ip_connect(self.method[0][1])
+        if self.method[0] == 'add':
+            self.ip_connect(self.method[1])
         else:
             self.network_search()
 
@@ -29,7 +29,7 @@ class AddFindDevices(threading.Thread):
 
 def main():
     if __name__ == "__main__":
-        add_find_device_thread = AddFindDevices('find')  # add value for the argument method 'add or find'
+        add_find_device_thread = AddFindDevices('add', '192.168.0.100')  # add value for the argument method 'add or find'
         add_find_device_thread.start()
 
 
