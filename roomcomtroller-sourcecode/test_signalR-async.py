a61bc4084e301a43119c614f7daab51dd2818b8a
cluemaster-robert <79128397+cluemaster-robert@users.noreply.github.com>
2022-11-07 09:32:20 -0800

    testing

--- roomcomtroller-sourcecode/test_signalR-async.py
+++ roomcomtroller-sourcecode/test_signalR-async.py
@@ -1,6 +1,6 @@
 import asyncio
 from signalr_async.netcore import Hub, Client
-from signalr_async.netcore.protocols import MessagePackProtocol
+from signalr_async.netcore.protocols import MessagePackProtocol, JsonProtocol
 
 
 class MyHub(Hub):
@@ -18,11 +18,11 @@ class MyHub(Hub):
 
     async def get_something(self) -> bool:
         """Invoke (method) on server"""
-        return await self.invoke("method", "arg1", 2)
+        return await self.invoke("Send2", "Hello Tesing", 2)
 
 
 hub = MyHub("chatHub")
-
+##hub = "chatHub"
 
 @hub.on("event_three")
 async def three(z: int) -> None:
@@ -43,15 +43,17 @@ for i in range(10):
 
 
 async def main():
-    token = "mytoken"
+    token = "F48C-5064-6347:c156e961919141723e5cb21c01647838cf5fc7f39b0a1bb31c9f4c1daeb4e348"
     headers = {"Authorization": f"Bearer {token}"}
     async with Client(
-        "https://devapi.cluemaster.io:9000",
+        "https://devapi.cluemaster.io",
         hub,
         connection_options={
             "http_client_options": {"headers": headers},
             "ws_client_options": {"headers": headers, "timeout": 1.0},
-            "protocol": MessagePackProtocol(),
+##            "protocol": MessagePackProtocol(),
+            "protocol": JsonProtocol(),
+##            "logger": logging.Logger(),
         },
     ) as client:
         return await hub.get_something()
